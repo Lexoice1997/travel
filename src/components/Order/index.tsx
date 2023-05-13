e6040be395cd12a95da84201c46ba6e72f519ede
Azamat <lexoice1997@gmail.com>
2023-05-14 00:39:51 +0500

    fix: order

--- src/components/Order/index.tsx
+++ src/components/Order/index.tsx
@@ -27,27 +27,35 @@ function Order() {
       <div className="bg-white p-8 rounded-xl">
         <div className="flex justify-between sm:flex-row flex-col items-start ">
           <div className="sm:w-1/2 w-full sm:mr-10 mr-0">
-            <div className="w-full mb-4">
-              <input type="text" className="border-gray-400 border rounded-md sm:w-1/2 w-full p-3 outline-none" />
-              <p className="text-gray-500 text-lg mt-4">
+            <div className="w-full mb-4 sm:mr-10 mr-0">
+              <input
+                type="text"
+                className="border-gray-400 border rounded-md w-full p-[14px] outline-none sm:mr-10 mr-0"
+                placeholder="+998972411997"
+              />
+              <p className="text-gray-500 text-lg mt-2">
                 What is your phone number?
               </p>
             </div>
-            <div className="sw-full sm:mr-10 mr-0">
+            <div className="w-full mb-4">
               <SelectField placeHolder="Location" options={LOCATIONS} />
-              <p className="text-gray-500 text-lg mt-4">
+              <p className="text-gray-500 text-lg mt-2">
                 Where do you wanna go?
               </p>
             </div>
           </div>
           <div className="sm:w-1/2 w-full">
             <div className="w-full mb-4">
-              <SelectField placeHolder="People" options={PEOPLES} />
-              <p className="text-gray-500 text-lg mt-4">How many people?</p>
+              <input
+                type="text"
+                className="border-gray-400 border rounded-md w-full p-[14px] outline-none sm:mr-10 mr-0"
+                placeholder="5"
+              />
+              <p className="text-gray-500 text-lg mt-2">How many people?</p>
             </div>
-            <div className="w-full">
+            <div className="w-full mt-2">
               <CustomDatePicker />
-              <p className="text-gray-500 text-lg mt-4">
+              <p className="text-gray-500 text-lg">
                 Where do you want to go there?
               </p>
             </div>
